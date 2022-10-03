f6bebe3f6f6b72b6ec89b7501be2fa077b193016
julietajimenez <romijimenez06@gmail.com>
2022-10-03 14:58:48 -0300

    merge con LoginContext

--- client/src/components/Brands/Revlon.jsx
+++ client/src/components/Brands/Revlon.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate} from "react-router-dom";
-import {removeDetail, pagesControl, getProductsByBrand, getAllProducts } from "../../redux/actions/ProductsActions";
+import { useNavigate } from "react-router-dom";
+import { removeDetail, pagesControl, getProductsByBrand, getAllProducts } from "../../redux/actions/ProductsActions";
 import Cards from "../Cards/Cards";
+import styles from '../Details/Detail.module.css'
 
-export default function Revlon (props){
+export default function Revlon(props) {
     const { onAddCarrito } = props;
-    const dispatch= useDispatch()
+    const dispatch = useDispatch()
     const navigate = useNavigate()
-    const product = useSelector(state=> state.products.allProducts)
-    const page = useSelector(state=> state.products.pages)
-    const labialRevlon = product.find(e=>e.id === '8580de77-bd8b-4c91-bcf6-f7e001c74933')
-console.log(labialRevlon);
+    const product = useSelector(state => state.products.allProducts)
+    const page = useSelector(state => state.products.pages)
+    const labialRevlon = product.find(e => e.id === '8580de77-bd8b-4c91-bcf6-f7e001c74933')
+    console.log(labialRevlon);
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getProductsByBrand('revlon'))
-        return ()=> {
+        return () => {
             dispatch(removeDetail())
         }
     }, [])
 
     return (
-        <div>
+        <div className={styles.container}>
 
-{/*                         <Cards
+            {/*                         <Cards
                         key={"8580de77-bd8b-4c91-bcf6-f7e001c74933"}
                           id={"8580de77-bd8b-4c91-bcf6-f7e001c74933"}
                           name={"Revlon Ultra HD Matte Lipcolor"}
@@ -36,18 +37,19 @@ console.log(labialRevlon);
                           
                           onAddCarrito={props.onAddCarrito} 
                         /> */}
-                                <div>
-            <h1>{"Revlon Ultra HD Matte Lipcolor"}</h1>
-            <h4>{"revlon"}</h4>
-            <img src={"https://d3t32hsnjxo7q6.cloudfront.net/i/8e5fad1f09c17ff8b312508a2a7da161_ra,w158,h184_pa,w158,h184.png"}/>
-            <h4>{"10.99"}</h4>
-            <h4>{"Revlon Ultra HD Matte Lipcolor gives you lightweight, high definition velvety matte color. It has a moisturizing, velvety \nfeel with a 100% wax-free, gel formula. Features: Creamy mango and whipped vanilla fragrance \nPlush velvety applicator Shade Range: (top row, left to right / bottom row, left to right) Devotion, Obsession, Addiction, Temptation / Flirtation, Love, Seduction, Passion"}</h4>
-            <button onClick={() => onAddCarrito(labialRevlon)}>Agregar al carrito</button>
-            
-        </div>
-            <button onClick={()=>{
-                                navigate(-1) 
-                                dispatch(pagesControl(page))}}>Volver</button> 
+            <div className={styles.description}>
+                <h1>{"Revlon Ultra HD Matte Lipcolor"}</h1>
+                <h4>{"revlon"}</h4>
+                <img src={"https://d3t32hsnjxo7q6.cloudfront.net/i/8e5fad1f09c17ff8b312508a2a7da161_ra,w158,h184_pa,w158,h184.png"} />
+                <h4>{"10.99"}</h4>
+                <p>{"Revlon Ultra HD Matte Lipcolor gives you lightweight, high definition velvety matte color. It has a moisturizing, velvety \nfeel with a 100% wax-free, gel formula. Features: Creamy mango and whipped vanilla fragrance \nPlush velvety applicator Shade Range: (top row, left to right / bottom row, left to right) Devotion, Obsession, Addiction, Temptation / Flirtation, Love, Seduction, Passion"}</p>
+                <button onClick={() => onAddCarrito(labialRevlon)}>Agregar al carrito</button>
+
+            </div>
+            <button onClick={() => {
+                navigate(-1)
+                dispatch(pagesControl(page))
+            }}>Volver</button>
         </div>
     )
 }
