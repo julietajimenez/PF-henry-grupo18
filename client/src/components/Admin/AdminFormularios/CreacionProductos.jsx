f1df4aeb26e58b41b9f5ef23d79c9c1cb9fe146b
julietajimenez <romijimenez06@gmail.com>
2022-10-03 14:31:55 -0300

    pull estilos

--- client/src/components/Admin/AdminFormularios/CreacionProductos.jsx
+++ client/src/components/Admin/AdminFormularios/CreacionProductos.jsx
@@ -1,5 +1,141 @@
-<<<<<<< HEAD
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCategorys } from "../../../redux/actions/CategorysActions";
+import { postProducts } from "../../../redux/actions/ProductsActions";
+import { useNavigate } from 'react-router-dom';
+import styles from "./CreacionProductos.module.css";
+
+function CreacionProductos() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const categories = useSelector((state) => state.categorys.categorys);
+  const [input, setInput] = useState({
+    name: "",
+    brand: "",
+    price: "",
+    description: "",
+    stock: "",
+    image: "",
+    category: "",
+  });
+  useEffect(() => {
+    dispatch(getAllCategorys());
+  }, []);
+  function handleChange(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSelect(e){
+    setInput({
+        ...input,
+        category: e.target.value
+    })
+}
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(postProducts(input));
+    alert("creado con exito");
+    setInput({
+      name: "",
+      brand: "",
+      price: "",
+      description: "",
+      stock: "",
+      image: "",
+      category: "",
+    });
+    navigate('/catalogo')
+  }
+  return (
+    <div className={styles.flexContainer}>
+      <form
+        className={styles.container}
+        autoComplete="off"
+        autoCapitalize="on"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <div className={styles.formInputs}>
+          <label>Nombre: </label>
+          <input
+          required
+            type="text"
+            name="name"
+            value={input.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.formInputs}>
+          <label>Marca: </label>
+          <input
+          required
+            type="text"
+            name="brand"
+            value={input.brand}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.formInputs}>
+          <label>Precio: </label>
+          <input
+          required
+            type="text"
+            name="price"
+            value={input.price}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.formInputs}>
+          <label>Imagen: </label>
+          <input
+            type="text"
+            name="image"
+            value={input.image}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.formInputs}>
+          <label>Stock: </label>
+          <input
+            type="number"
+            name="stock"
+            value={input.stock}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.formInputs}>
+          <label>Descripción: </label>
+          <textarea
+            type="text"
+            name="description"
+            value={input.description}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <div className={styles.formInputs}>
+          <select onChange={(e) => handleSelect(e)}>
+            {categories &&
+              categories.map((e) => {
+                return <option value={e}>{e}</option>;
+              })}
+          </select>
+        </div>
+        <button className={styles.btnLogin} type="submit">
+          ENVIAR
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default CreacionProductos;
+
+/* import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
@@ -24,41 +160,14 @@ useEffect(()=>{
     dispatch(getAllCategorys())
 }, [])
 function handleChange (e){
-=======
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllCategorys } from "../../../redux/actions/CategorysActions";
-import { postProducts } from "../../../redux/actions/ProductsActions";
-import styles from "./CreacionProductos.module.css";
-
-function CreacionProductos() {
-  const dispatch = useDispatch();
-  const categories = useSelector((state) => state.categorys.categorys);
-  const [input, setInput] = useState({
-    name: "",
-    marca: "",
-    price: "",
-    description: "",
-    stock: "",
-    image: "",
-    category: "",
-  });
-  useEffect(() => {
-    dispatch(getAllCategorys());
-  }, []);
-  function handleChange(e) {
->>>>>>> 3de8eed4453dc9228943161adce1fbf2e043efea
     setInput({
-      ...input,
-      [e.target.name]: e.target.value,
-    });
-  }
+        ...input, 
+        [e.target.name]: e.target.value
+    })
+}
 
-  function handleSelect(e) {
+function handleSelect(e){
     setInput({
-<<<<<<< HEAD
         ...input,
         category: e.target.value
     })
@@ -120,104 +229,4 @@ function handleSubmit (e){
                 </select>
             </div>  
             <button type='submit'>ENVIAR</button>
-        </form>
-=======
-      ...input,
-      category: [...new Set([...input.category, e.target.value])],
-    });
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    dispatch(postProducts(input));
-    alert("creado con exito");
-    setInput({
-      name: "",
-      marca: "",
-      price: "",
-      description: "",
-      stock: "",
-      image: "",
-      category: "",
-    });
-  }
-  return (
-    <div className={styles.flexContainer}>
-      <form
-        className={styles.container}
-        autoComplete="off"
-        autoCapitalize="on"
-        onSubmit={(e) => handleSubmit(e)}
-      >
-        <div className={styles.formInputs}>
-          <label>Nombre: </label>
-          <input
-            type="text"
-            name="name"
-            value={input.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div className={styles.formInputs}>
-          <label>Marca: </label>
-          <input
-            type="text"
-            name="marca"
-            value={input.marca}
-            onChange={handleChange}
-          />
-        </div>
-        <div className={styles.formInputs}>
-          <label>Precio: </label>
-          <input
-            type="text"
-            name="price"
-            value={input.price}
-            onChange={handleChange}
-          />
-        </div>
-        <div className={styles.formInputs}>
-          <label>Imagen: </label>
-          <input
-            type="text"
-            name="image"
-            value={input.image}
-            onChange={handleChange}
-          />
-        </div>
-        <div className={styles.formInputs}>
-          <label>Stock: </label>
-          <input
-            type="number"
-            name="stock"
-            value={input.stock}
-            onChange={handleChange}
-          />
-        </div>
-        <div className={styles.formInputs}>
-          <label>Descripción: </label>
-          <textarea
-            type="text"
-            name="description"
-            value={input.description}
-            onChange={handleChange}
-          ></textarea>
-        </div>
-        <div className={styles.formInputs}>
-          <select onChange={(e) => handleSelect(e)}>
-            {categories &&
-              categories.map((e) => {
-                return <option value={e}>{e}</option>;
-              })}
-          </select>
-        </div>
-        <button className={styles.btnLogin} type="submit">
-          ENVIAR
-        </button>
-      </form>
->>>>>>> 3de8eed4453dc9228943161adce1fbf2e043efea
-    </div>
-  );
-}
-
-export default CreacionProductos;
+        </form> */
--- client/src/components/Details/Detail.jsx
+++ client/src/components/Details/Detail.jsx
@@ -2,48 +2,13 @@ import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
-<<<<<<< HEAD
-import { productById, removeDetail, pagesControl } from "../../redux/actions/ProductsActions";
-import imgDefault from './imageDefault.jpg'
-
-export default function Detail (){
-    const {id} = useParams()
-    const dispatch= useDispatch()
-    const navigate = useNavigate()
-    const product = useSelector(state=> state.products.detail)
-    const page = useSelector(state=> state.products.pages)
-console.log(page);
-    useEffect(()=>{
-        dispatch(productById(id))
-        return ()=> {
-            dispatch(removeDetail())
-        }
-    }, [id])
-console.log(product);
-    return (
-        product.name? 
-        <div>
-            <h1>{product.name}</h1>
-            { product.image? <img src={product.image} alt="nohayimagen" /> : <img style={{height:'270px'}} src={imgDefault}/>}
-            <h4>${product.price}USD</h4>
-            <h4>{product.description}</h4>
-            {
-                product.stock < 2 ? <h4>¡Último producto disponible!</h4> : null
-            }
-            <button onClick={()=>{
-                                navigate(-1) 
-                                dispatch(pagesControl(page))}}>Volver</button> 
-        </div> :
-        <h1>CARGANDO...</h1>
-    )
-}
-=======
 import {
   productById,
   removeDetail,
   pagesControl,
 } from "../../redux/actions/ProductsActions";
 import styles from "./Detail.module.css";
+import imgDefault from './imageDefault.jpg'
 
 export default function Detail() {
   const { id } = useParams();
@@ -63,7 +28,8 @@ export default function Detail() {
     <div className={styles.container}>
       <div className={styles.description}>
         <h2>{product.name}</h2>
-        <img src={product.image} alt="Imagen del producto" />
+        { product.image? <img src={product.image} alt="nohayimagen" /> : <img style={{height:'270px'}} src={imgDefault}/>}
+            <h4>${product.price}USD</h4>
 
         <p>{product.description}</p>
         {product.stock < 2 ? <h4>¡Último producto disponible!</h4> : null}
@@ -82,4 +48,3 @@ export default function Detail() {
     <h1>CARGANDO...</h1>
   );
 }
->>>>>>> 3de8eed4453dc9228943161adce1fbf2e043efea
