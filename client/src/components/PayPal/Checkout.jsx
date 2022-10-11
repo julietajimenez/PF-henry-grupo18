1093b0ca2b756204b8abca79926310e9db29f514
gguidoxx <gudombe@gmail.com>
2022-10-11 00:38:40 -0300

    Cloudinary Solucionado, cambios en checkout

--- client/src/components/PayPal/Checkout.jsx
+++ client/src/components/PayPal/Checkout.jsx
@@ -4,7 +4,7 @@ import swal from "sweetalert";
 import { Link, useNavigate } from "react-router-dom";
 import sentEmail from "./Firebase/sentEmail";
 
-function Checkout({ descripcion }) {
+function Checkout({}) {
   const logueado = JSON.parse(localStorage.logueado);
   const checkoutinfo = JSON.parse(localStorage.getItem("carrito"));
   let precio = checkoutinfo.map((e) => e);
@@ -12,17 +12,19 @@ function Checkout({ descripcion }) {
     .map((e) => e.cantidad * e.price)
     .reduce((a, b) => a + b, 0);
 
+  const descripcion = precio.map((e) => (
+    <li key={e.id}>{e.name + ": por " + "$" + e.price + " c/u"}</li>
+  ));
+
   function submitHandler() {
     let email = logueado.email; // ASIGNO EL VALOR DE CORREO SEGÚN LO ENVIADO POR INPUT
     let subject = "¡Gracias por tu compra en nuestra tienda!"; // LO MISMO
-    let body = descripcion + "por un valor total de: " + "valor"; // DE ARRIBA
+    let body = descripcion + "por un valor total de: " + valor; // DE ARRIBA
     sentEmail(email, subject, body); // EJECUTO LA FUNCIÓN SENTEMAIL Y LE ENVÍO LOS DATOS POR PROPS... INVESTIGAR DE COMO INCORPORAR ESTO AL ATRIBUT ONAPPROVE DE PAYPAL
   }
 
   const [paidFor, setPaidFor] = useState(false);
   const [error, setError] = useState(null);
-
-
 
   const REACT_APP_PAYPAL_CLIENT_ID =
     "ATbqit3pbxakLWH6-Sgyh_FIaRvtCBr-Vkq2AWChyWBBx4monf1Dumry1mGcdZiYLgxN4TvdaPGzKk6l";
@@ -39,11 +41,13 @@ function Checkout({ descripcion }) {
   }
 
   return (
-    <div style={{minHeight:"70vh"}}>
+    <div style={{ minHeight: "70vh" }}>
       <h1>¡Gracias por su compra!</h1>
       <div>
         Esto es lo que usted estará comprando:{" "}
-        {precio.map((e) => <li key={e.id}>{e.name + ': por ' + '$' +e.price + ' c/u'}</li>)}
+        {precio.map((e) => (
+          <li key={e.id}>{e.name + ": por " + "$" + e.price + " c/u"}</li>
+        ))}
         Total será de: ${valor}
       </div>
       <PayPalScriptProvider
