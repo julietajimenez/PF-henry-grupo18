580c03ce4593a1d2ca12bc6a1a4eedcb2d0a2236
gguidoxx <gudombe@gmail.com>
2022-10-14 22:22:33 -0300

    Cambios deploy

--- client/src/components/Admin/AdminFormularios/UpdateUsers.jsx
+++ client/src/components/Admin/AdminFormularios/UpdateUsers.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
